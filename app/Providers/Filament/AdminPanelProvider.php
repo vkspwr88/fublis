972407e3131c8a98ffc7d39547e3c934e981f116
Aman Saini <amansaini87@rediffmail.com>
2023-09-29 23:51:24 +0530

    uncomment vitetheme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => '#6941C6'/* Color::Amber */,
             ])
-			//->viteTheme('resources/css/filament/backend/theme.css')
+			->viteTheme('resources/css/filament/backend/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
