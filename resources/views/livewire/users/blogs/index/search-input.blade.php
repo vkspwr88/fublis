2b9c7b841d8116b79c3903b70179d872cc6f7420
Aman Saini <amansaini87@rediffmail.com>
2023-10-06 18:08:26 +0530

    implemented hide search result box

--- resources/views/livewire/users/blogs/index/search-input.blade.php
+++ resources/views/livewire/users/blogs/index/search-input.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="click-text">
     <div class="input-group mb-4">
 		<label class="input-group-text bg-white" for="blogSearchInput"><i class="bi bi-search"></i></label>
 		<input id="blogSearchInput" class="form-control border-start-0 shadow-none ps-0" type="search" placeholder="Search by name" aria-label="Search" wire:model="searchInput" wire:keydown.enter="searchPosts" wire:focus="$set('showSearchBox', true)" />
@@ -14,61 +14,61 @@
 			@else
 				<div>
 					@if(empty($postsByName) && empty($postsByAuthor) && empty($postsByTags))
-					No result found
+						No result found
 					@else
 						@if(!empty($postsByName))
-						<h6 class="m-0 bg-purple-800 text-white p-2 rounded">Title</h6>
-						<ul class="list-group list-group-flush">
-							@forelse ($postsByName as $blog)
-							<li class="list-group-item d-flex justify-content-between align-items-start px-0">
-								<div class="ms-2 me-auto w-100">
-									<div class="fw-bold text-truncate">{{ $blog->title }}</div>
-									<div class="text-truncate">{{ $blog->description }}</div>
-									<a href="{{ route('blogs.show', ['slug' => $blog->slug]) }}" class="stretched-link"></a>
-								</div>
-							</li>
-							@empty
-							<li class="list-group-item d-flex justify-content-between align-items-start px-0 text-muted">
-								No result found
-							</li>
-							@endforelse
-						</ul>
+							<h6 class="m-0 bg-purple-800 text-white p-2 rounded">Title</h6>
+							<ul class="list-group list-group-flush">
+								@forelse ($postsByName as $blog)
+								<li class="list-group-item d-flex justify-content-between align-items-start px-0">
+									<div class="ms-2 me-auto w-100">
+										<div class="fw-bold text-truncate">{{ $blog->title }}</div>
+										<div class="text-truncate">{{ $blog->description }}</div>
+										<a href="{{ route('blogs.show', ['slug' => $blog->slug]) }}" class="stretched-link"></a>
+									</div>
+								</li>
+								@empty
+								<li class="list-group-item d-flex justify-content-between align-items-start px-0 text-muted">
+									No result found
+								</li>
+								@endforelse
+							</ul>
 						@endif
 						@if(!empty($postsByAuthor))
-						<h6 class="m-0 bg-purple-800 text-white p-2 rounded">Authors</h6>
-						<ul class="list-group list-group-flush">
-							@forelse ($postsByAuthor as $blog)
-							<li class="list-group-item d-flex justify-content-between align-items-start px-0">
-								<div class="ms-2 me-auto w-100">
-									<div class="fw-bold text-truncate">{{ $blog->title }}</div>
-									<div class="text-truncate">{{ $blog->description }}</div>
-									<a href="{{ route('blogs.show', ['slug' => $blog->slug]) }}" class="stretched-link"></a>
-								</div>
-							</li>
-							@empty
-							<li class="list-group-item d-flex justify-content-between align-items-start px-0 text-muted">
-								No result found
-							</li>
-							@endforelse
-						</ul>
+							<h6 class="m-0 bg-purple-800 text-white p-2 rounded">Authors</h6>
+							<ul class="list-group list-group-flush">
+								@forelse ($postsByAuthor as $blog)
+								<li class="list-group-item d-flex justify-content-between align-items-start px-0">
+									<div class="ms-2 me-auto w-100">
+										<div class="fw-bold text-truncate">{{ $blog->title }}</div>
+										<div class="text-truncate">{{ $blog->description }}</div>
+										<a href="{{ route('blogs.show', ['slug' => $blog->slug]) }}" class="stretched-link"></a>
+									</div>
+								</li>
+								@empty
+								<li class="list-group-item d-flex justify-content-between align-items-start px-0 text-muted">
+									No result found
+								</li>
+								@endforelse
+							</ul>
 						@endif
 						@if(!empty($postsByTags))
-						<h6 class="m-0 bg-purple-800 text-white p-2 rounded">Tags</h6>
-						<ul class="list-group list-group-flush">
-							@forelse ($postsByTags as $blog)
-							<li class="list-group-item d-flex justify-content-between align-items-start px-0">
-								<div class="ms-2 me-auto w-100">
-									<div class="fw-bold text-truncate">{{ $blog->title }}</div>
-									<div class="text-truncate">{{ $blog->description }}</div>
-									<a href="{{ route('blogs.show', ['slug' => $blog->slug]) }}" class="stretched-link"></a>
-								</div>
-							</li>
-							@empty
-							<li class="list-group-item d-flex justify-content-between align-items-start px-0 text-muted">
-								No result found
-							</li>
-							@endforelse
-						</ul>
+							<h6 class="m-0 bg-purple-800 text-white p-2 rounded">Tags</h6>
+							<ul class="list-group list-group-flush">
+								@forelse ($postsByTags as $blog)
+								<li class="list-group-item d-flex justify-content-between align-items-start px-0">
+									<div class="ms-2 me-auto w-100">
+										<div class="fw-bold text-truncate">{{ $blog->title }}</div>
+										<div class="text-truncate">{{ $blog->description }}</div>
+										<a href="{{ route('blogs.show', ['slug' => $blog->slug]) }}" class="stretched-link"></a>
+									</div>
+								</li>
+								@empty
+								<li class="list-group-item d-flex justify-content-between align-items-start px-0 text-muted">
+									No result found
+								</li>
+								@endforelse
+							</ul>
 						@endif
 					@endif
 				</div>
@@ -80,6 +80,27 @@
 
 @stack('script')
 <script>
+	const concernedElement = document.querySelector(".click-text");
+	const searchBox = $('#searchBox');
+
+	document.addEventListener('livewire:initialized', () => {
+		//console.log(Livewire.getByName('users.blogs.index.search-input')[0].get('showSearchBox'));
+		//console.log($wire);
+	});
+
+	document.addEventListener("mousedown", (event) => {
+		if (concernedElement.contains(event.target)) {
+			//searchBox.show();
+			//console.log("Clicked Inside");
+		} else {
+			//console.log("Clicked Outside / Elsewhere");
+			const showSearchBox = Livewire.getByName('users.blogs.index.search-input')[0].get('showSearchBox');
+			if(showSearchBox){
+				//searchBox.hide();
+				Livewire.getByName('users.blogs.index.search-input')[0].set('showSearchBox', false);
+			}
+		}
+	});
 	/* const searchBox = $('#searchBox');
 	searchBox.hide();
 
