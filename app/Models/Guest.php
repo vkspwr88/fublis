8ce48d53034167dedb0dc6ec9d980af9baf9ef1f
Aman Saini <amansaini87@rediffmail.com>
2023-10-24 15:41:30 +0530

    added guest table

---
+++ app/Models/Guest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Casts\EmailCast;
+use App\Casts\NameCast;
+use App\Casts\Users\UserTypeCast;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+
+class Guest extends Model
+{
+    use HasFactory;
+
+	protected $fillable = [
+		'name',
+		'email',
+		'password',
+		'user_type',
+	];
+
+	protected $hidden = [
+		'password',
+	];
+
+	protected $casts = [
+		'name' => NameCast::class,
+		'email' => EmailCast::class,
+		'email_verified_at' => 'datetime',
+		'password' => 'hashed',
+		'user_type' => UserTypeCast::class,
+	];
+
+	public function setPasswordAttribute($value)
+	{
+		$this->attributes['password'] = Hash::make($value);
+	}
+}
