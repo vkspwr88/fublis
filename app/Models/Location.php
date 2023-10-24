52c2eb3572c5429d6daa6c5493821b472120c183
Aman Saini <amansaini87@rediffmail.com>
2023-10-24 12:37:18 +0530

    fixed location module

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
+use App\Casts\NameCast;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Location extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, HasUuids;
 
 	protected $guarded = [];
+
+	protected $casts = [
+		'name' => NameCast::class,
+	];
 }
