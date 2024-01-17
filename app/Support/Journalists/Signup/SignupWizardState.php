75c3efe0e8dfc9879c79cffad00f7139383cf40c
Aman Saini <amansaini87@rediffmail.com>
2024-01-17 20:38:37 +0530

    fixing google login error

--- app/Support/Journalists/Signup/SignupWizardState.php
+++ app/Support/Journalists/Signup/SignupWizardState.php
@@ -10,7 +10,7 @@ class SignupWizardState extends State
 	{
 		$journalistSignupStepState = $this->forStep('journalist-signup-step');
 		return [
-			'email' => $journalistSignupStepState['email'],
+			// 'email' => $journalistSignupStepState['email'],
 			'password' => $journalistSignupStepState['password'],
 		];
 	}
