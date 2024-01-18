0bc514817b775ca8f80b16525be9f9d9da22c9d6
Aman Saini <amansaini87@rediffmail.com>
2024-01-18 12:32:26 +0530

    implemented feedback task #1

--- resources/views/emails/user/architect/signup/verification-email.blade.php
+++ resources/views/emails/user/architect/signup/verification-email.blade.php
@@ -1,13 +1,13 @@
 @extends('emails.layouts.master')
 
 @section('body')
-<p>Hi {{ $guest->name }},</p>
-<p>This is your verification code:</p>
-<div>
-<p class="otp-box">{{ $otp[0] }}</p>
-<p class="otp-box">{{ $otp[1] }}</p>
-<p class="otp-box">{{ $otp[2] }}</p>
-<p class="otp-box">{{ $otp[3] }}</p>
+<p class="mb-3">Hi {{ $guest->name }},</p>
+<p class="mb-3">This is your verification code:</p>
+<div class="mb-3">
+<p class="otp-box"><span>{{ $otp[0] }}</span></p>
+<p class="otp-box"><span>{{ $otp[1] }}</span></p>
+<p class="otp-box"><span>{{ $otp[2] }}</span></p>
+<p class="otp-box" style="margin-right: 0;"><span>{{ $otp[3] }}</span></p>
 </div>
 <p>This code will only be valid for the next {{ $emailVerificationTimeout }} minutes.{{-- If the code does not work, you can use this login verification link: --}}</p>
 @endsection
