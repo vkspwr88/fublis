accfa87169ce23243537f030cf1e32074f1c6d66
Aman Saini <amansaini87@rediffmail.com>
2024-01-19 13:36:31 +0530

    implemented feedback 17-01-2023 task 19

--- resources/views/users/includes/common/profile-article-download-journalist.blade.php
+++ resources/views/users/includes/common/profile-article-download-journalist.blade.php
@@ -2,6 +2,24 @@
 	@if($downloadRequest->request_status === App\Enums\Users\Architects\MediaKits\RequestStatusEnum::APPROVED)
 		<div class="row align-items-center">
 			<div class="col">
+				<p class="text-dark fs-6 m-0">Company Profile</p>
+			</div>
+			<div class="col text-end">
+				@if ($mediaKit->story->article_doc_path)
+					<form class="m-0 p-0" action="{{ route('architect.download', ['mediaKit' => $mediaKit->slug]) }}" method="post">
+						@csrf
+						<input type="hidden" value="{{ $mediaKit->story->company_profile_path }}" name="file">
+						<input type="hidden" name="type" value="CompanyProfile">
+						<button type="submit" class="btn btn-primary fs-6 fw-medium">Download</button>
+					</form>
+				@else
+					<a class="btn btn-primary fs-6 fw-medium" href="{{ $mediaKit->story->company_profile_link }}" target="_blank">Download</a>
+				@endif
+			</div>
+		</div>
+		<hr class="border-gray-300">
+		<div class="row align-items-center">
+			<div class="col">
 				<p class="text-dark fs-6 m-0">Description</p>
 			</div>
 			<div class="col text-end">
