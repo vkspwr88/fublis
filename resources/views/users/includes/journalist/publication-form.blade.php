2032528b98cbb55709a7eec997eaf9e7b1c90b45
Aman Saini <amansaini87@rediffmail.com>
2024-02-01 01:16:53 +0530

    fixed character count for the about me

--- resources/views/users/includes/journalist/publication-form.blade.php
+++ resources/views/users/includes/journalist/publication-form.blade.php
@@ -186,7 +186,7 @@
 		<label class="d-block form-text text-secondary fs-7 m-0">Write short introduction about publication.</label>
 	</div>
 	<div class="col-md-8">
-		<textarea id="inputAboutMe" class="form-control @error('aboutMe') is-invalid @enderror" wire:model.live="aboutMe" rows="6"></textarea>
+		<textarea id="inputAboutMe" class="form-control @error('aboutMe') is-invalid @enderror" wire:model="aboutMe" wire:keydown.debounce="characterCount" rows="6"></textarea>
 		@error('aboutMe')<div class="invalid-feedback">{{ $message }}</div>@enderror
 		<div id="aboutMeHelp" class="form-text {{ $aboutMeLength < 0 ? 'text-danger' : '' }}">{{ $aboutMeLength }} characters left</div>
 	</div>
