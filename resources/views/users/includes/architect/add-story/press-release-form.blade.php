f03c9ae866fd3cda176ccdde34cfb08a8c45c2b1
Aman Saini <amansaini87@rediffmail.com>
2024-04-29 18:51:43 +0530

    fixing editor single quote problem

--- resources/views/users/includes/architect/add-story/press-release-form.blade.php
+++ resources/views/users/includes/architect/add-story/press-release-form.blade.php
@@ -260,8 +260,8 @@
 	</div>
 	@include('users.includes.common.file-upload-script', ['width' => 800, 'height' => 400])
 	<script>
-		const editor = document.querySelector("trix-editor");
-		editor.editor.insertHTML(`{!! $form->pressReleaseWrite !!}`);
+		const trixEditor = document.querySelector("trix-editor");
+		trixEditor.editor.insertHTML(`{!! $form->pressReleaseWrite !!}`);
 	</script>
 	@include('users.includes.architect.add-story.modal-delete-media-kit')
 </form>
