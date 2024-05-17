d63a383fb0793644f29c1de6607ea0885930e516
Aman Saini <amansaini87@rediffmail.com>
2024-05-18 00:45:06 +0530

    checking notification

--- resources/views/users/includes/journalist/notification/receive-media-kit-notification.blade.php
+++ resources/views/users/includes/journalist/notification/receive-media-kit-notification.blade.php
@@ -15,19 +15,18 @@
 		<img class="img-square rounded-circle img-48" src="{{ $profileImg }}" alt="..." />
 	</div>
 	@if ($notification->notifiable->pitchable instanceof App\Models\Call)
-		<div class="col-auto">
+		<div class="col-md text-truncate">
 			<p class="p-0 m-0">
 				<span class="text-dark fw-semibold">{{ $notification->notifiable->mediaKit->architect->user->name }}</span>
 				<span class="text-secondary">{{ formatDateTime($notification->created_at) }}</span>
 			</p>
-			<p class="p-0 m-0">{{ Str::limit(Str::replaceMatches(pattern: '/(<([^>]+)>)/', replace: '', subject: $notification->message
-				), 100, ' ...') }}</p>
+			<p class="p-0 m-0">{!! $notification->message !!}</p>
 		</div>
-		<div class="col text-end">
+		<div class="col-md-auto text-end">
 			<a href="{{ route('journalist.media-kit.view', ['mediaKit' => $notification->notifiable->mediaKit->slug]) }}" class="btn btn-primary">View Media Kit</a>
 		</div>
 	@else
-		<div class="col text-truncate">
+		<div class="col-md text-truncate">
 			<p class="p-0 m-0">
 				<span class="text-dark fw-semibold">{{ $notification->notifiable->mediaKit->architect->user->name }}</span>
 				<span class="text-secondary">{{ formatDateTime($notification->created_at) }}</span>
@@ -38,7 +37,7 @@
 				), 100, ' ...') }}"
 			</p>
 		</div>
-		<div class="col text-end">
+		<div class="col-md-auto text-end">
 			<a href="{{ route('journalist.media-kit.view', ['mediaKit' => $notification->notifiable->mediaKit->slug]) }}" class="btn btn-primary fs-6 fw-medium">View Media Kit</a>
 			<a href="{{ route('journalist.account.profile.message.index', ['chat' => $notification->notifiable->mediaKit->slug]) }}" class="btn btn-primary fs-6 fw-medium">Read Full Message</a>
 		</div>
