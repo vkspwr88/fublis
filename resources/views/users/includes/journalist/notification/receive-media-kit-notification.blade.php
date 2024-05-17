66d9047238b70879abbc2293e43a390c71f3cad6
Aman Saini <amansaini87@rediffmail.com>
2024-05-18 00:06:15 +0530

    checking notification

--- resources/views/users/includes/journalist/notification/receive-media-kit-notification.blade.php
+++ resources/views/users/includes/journalist/notification/receive-media-kit-notification.blade.php
@@ -41,7 +41,8 @@
 				<span class="text-dark fw-semibold">{{ $notification->notifiable->mediaKit->architect->user->name }}</span>
 				<span class="text-secondary">{{ formatDateTime($notification->created_at) }}</span>
 			</p>
-			<p class="p-0 m-0">{!! $notification->message !!}</p>
+			<p class="p-0 m-0">{{ str($notification->message)->replace(/(<([^>]+)>)/gi) }}</p>
+			{{-- <p class="p-0 m-0">{!! $notification->message !!}</p> --}}
 		</div>
 		<div class="col text-end">
 			<a href="{{ route('journalist.media-kit.view', ['mediaKit' => $notification->notifiable->mediaKit->slug]) }}" class="btn btn-primary">View Media Kit</a>
