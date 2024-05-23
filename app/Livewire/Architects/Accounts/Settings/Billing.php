4c55e201e4d66706442e4b06d67d76f0ea3475c4
Aman Saini <amansaini87@rediffmail.com>
2024-05-23 18:54:00 +0530

    testing stripe

--- app/Livewire/Architects/Accounts/Settings/Billing.php
+++ app/Livewire/Architects/Accounts/Settings/Billing.php
@@ -24,7 +24,7 @@ class Billing extends Component
 	public function mount()
 	{
 		$user = auth()->user();
-		dd($user->subscribed('default'), isBusinessPlanSubscribed(), isEnterprisePlanSubscribed(), $user->subscribedToProduct('prod_Q4ny3KlIL7WAgX', 'default'), $user->subscribedToPrice('price_1PEeMjSF38t8VQrgcCfGfFpm', 'default'));
+		dd(request()->user(), $user->subscribed('default'), isBusinessPlanSubscribed(), isEnterprisePlanSubscribed(), $user->subscribedToProduct('prod_Q4ny3KlIL7WAgX', 'default'), $user->subscribedToPrice('price_1PEeMjSF38t8VQrgcCfGfFpm', 'default'));
 		$this->userCount = CompanyController::getMediaContacts()->count();
 		$latestSubscription = $user?->latestSubscription;
 		// dd($latestSubscription);
