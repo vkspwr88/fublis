4ad107a9eb973633e9f51b69dda299e797050fae
Aman Saini <amansaini87@rediffmail.com>
2024-05-30 17:28:45 +0530

    fixing deleted publication bug

--- resources/views/components/users/journalists/full-list.blade.php
+++ resources/views/components/users/journalists/full-list.blade.php
@@ -2,7 +2,7 @@
     @if($journalists->count() > 0)
 		<div class="row g-4">
 			@foreach ($journalists as $journalist)
-				@if($journalist->publications[0])
+				@if($journalist->publications)
                 	<x-users.journalists.card :journalist="$journalist" />
 				@endif
 			@endforeach
