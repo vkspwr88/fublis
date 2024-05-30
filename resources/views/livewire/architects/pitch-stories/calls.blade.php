ff57eeab258abdfb7a129d1b07abab2a1a533129
Aman Saini <amansaini87@rediffmail.com>
2024-05-30 17:17:54 +0530

    fixing no publication access bug

--- resources/views/livewire/architects/pitch-stories/calls.blade.php
+++ resources/views/livewire/architects/pitch-stories/calls.blade.php
@@ -52,84 +52,86 @@
 		@if($calls->count() > 0)
 		<div class="row g-4">
 			@foreach ($calls as $call)
-			<div class="col-12" :key="$call->id">
-				<div class="bg-white border-0 shadow card rounded-3">
-					<div class="card-body">
-						<div class="row">
-							<div class="col-sm-12">
-								<div class="pb-3 row align-items-center g-3">
-									<div class="col-md-7">
-										<div class="text-center row justify-content-center align-items-center text-md-start">
-											<p class="m-0 col-12 fs-5 fw-semibold">
-												<a href="{{ route('architect.pitch-story.calls.view', ['call' => $call->slug]) }}" class="text-dark">
-													{{ $call->title }}
-												</a>
-											</p>
-											<p class="m-0 col-12 text-secondary fs-6">Deadline: {{ formatDate($call->submission_end_date) }}</p>
-										</div>
-									</div>
-									<div class="col-md-5">
-										<div class="row justify-content-center align-items-end">
-											<p class="m-0 text-center col text-md-end fs-6">
-												<button type="button" class="btn btn-primary btn-sm fw-medium" wire:click="showMediaKit('{{ $call->id }}')">
-													Submit Story <x-users.spinners.white-btn wire:target="showMediaKit('{{ $call->id }}')" />
-												</button>
-											</p>
-										</div>
-									</div>
-								</div>
-								<div class="row justify-content-center align-items-end g-3">
-									<div class="col-md-6">
-										<div class="row justify-content-end">
-											<div class="col-12">
-												<div class="row justify-content-center justify-content-md-start align-items-center">
-													<div class="col-auto">
-														@php
-															$profileImg = $call->publication->profileImage ?
-																			Storage::url($call->publication->profileImage->image_path) :
-																			App\Http\Controllers\Users\AvatarController::setProfileAvatar([
-																				'name' => $call->publication->name,
-																				'width' => 45,
-																				'fontSize' => 18,
-																				'background' => $call->publication->background_color,
-																				'foreground' => $call->publication->foreground_color,
-																			], 'publication');
-														@endphp
-														<img src="{{ $profileImg }}" class="img-sqaure img-45 rounded-circle" alt="..." />
-													</div>
-													<div class="col-auto fs-6">
-														<p class="p-0 m-0 fw-semibold">
-															<a class="text-secondary" href="{{ route('architect.pitch-story.publications.view', ['publication' => $call->publication->slug]) }}">
-																{{ $call->publication->name }}
-															</a>
-														</p>
-														<p class="p-0 m-0 small">
-															<a class="text-secondary" href="{{ route('architect.pitch-story.journalists.view', ['journalist' => $call->journalist->slug]) }}">
-																{{ $call->journalist->user->name }}
-															</a>
-														</p>
-													</div>
+				@if($call->publication)
+					<div class="col-12" :key="$call->id">
+						<div class="bg-white border-0 shadow card rounded-3">
+							<div class="card-body">
+								<div class="row">
+									<div class="col-sm-12">
+										<div class="pb-3 row align-items-center g-3">
+											<div class="col-md-7">
+												<div class="text-center row justify-content-center align-items-center text-md-start">
+													<p class="m-0 col-12 fs-5 fw-semibold">
+														<a href="{{ route('architect.pitch-story.calls.view', ['call' => $call->slug]) }}" class="text-dark">
+															{{ $call->title }}
+														</a>
+													</p>
+													<p class="m-0 col-12 text-secondary fs-6">Deadline: {{ formatDate($call->submission_end_date) }}</p>
+												</div>
+											</div>
+											<div class="col-md-5">
+												<div class="row justify-content-center align-items-end">
+													<p class="m-0 text-center col text-md-end fs-6">
+														<button type="button" class="btn btn-primary btn-sm fw-medium" wire:click="showMediaKit('{{ $call->id }}')">
+															Submit Story <x-users.spinners.white-btn wire:target="showMediaKit('{{ $call->id }}')" />
+														</button>
+													</p>
 												</div>
 											</div>
 										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="flex-wrap d-flex justify-content-center justify-content-md-end align-items-center fw-medium">
-											<span class="mb-1 text-purple-700 badge rounded-pill bg-purple-50">
-												{{ $call->language->name }}
-											</span>
-											<span class="mb-1 text-purple-700 badge rounded-pill bg-purple-50">Project</span>
-											<span class="mb-1 text-purple-700 badge rounded-pill bg-purple-50">
-												{{ $call->category->name }}
-											</span>
+										<div class="row justify-content-center align-items-end g-3">
+											<div class="col-md-6">
+												<div class="row justify-content-end">
+													<div class="col-12">
+														<div class="row justify-content-center justify-content-md-start align-items-center">
+															<div class="col-auto">
+																@php
+																	$profileImg = $call->publication->profileImage ?
+																					Storage::url($call->publication->profileImage->image_path) :
+																					App\Http\Controllers\Users\AvatarController::setProfileAvatar([
+																						'name' => $call->publication->name,
+																						'width' => 45,
+																						'fontSize' => 18,
+																						'background' => $call->publication->background_color,
+																						'foreground' => $call->publication->foreground_color,
+																					], 'publication');
+																@endphp
+																<img src="{{ $profileImg }}" class="img-sqaure img-45 rounded-circle" alt="..." />
+															</div>
+															<div class="col-auto fs-6">
+																<p class="p-0 m-0 fw-semibold">
+																	<a class="text-secondary" href="{{ route('architect.pitch-story.publications.view', ['publication' => $call->publication->slug]) }}">
+																		{{ $call->publication->name }}
+																	</a>
+																</p>
+																<p class="p-0 m-0 small">
+																	<a class="text-secondary" href="{{ route('architect.pitch-story.journalists.view', ['journalist' => $call->journalist->slug]) }}">
+																		{{ $call->journalist->user->name }}
+																	</a>
+																</p>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="flex-wrap d-flex justify-content-center justify-content-md-end align-items-center fw-medium">
+													<span class="mb-1 text-purple-700 badge rounded-pill bg-purple-50">
+														{{ $call->language->name }}
+													</span>
+													<span class="mb-1 text-purple-700 badge rounded-pill bg-purple-50">Project</span>
+													<span class="mb-1 text-purple-700 badge rounded-pill bg-purple-50">
+														{{ $call->category->name }}
+													</span>
+												</div>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+				@endif
 			@endforeach
 		</div>
 		{{ $calls->links('vendor.livewire.custom-pagination') }}
