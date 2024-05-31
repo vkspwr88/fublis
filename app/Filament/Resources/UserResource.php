284de48c9b5ccd17b827a3cf2605f75c2dc92d8f
Aman Saini <amansaini87@rediffmail.com>
2024-05-31 21:10:23 +0530

    fixed unique email address for user in backend

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -29,7 +29,7 @@ class UserResource extends Resource
                     ->maxLength(255),
                 Forms\Components\TextInput::make('email')
                     ->email()
-					->unique()
+					->unique(ignoreRecord: true)
                     ->required()
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at')
@@ -37,7 +37,7 @@ class UserResource extends Resource
 					->default(now()),
                 Forms\Components\TextInput::make('password')
                     ->password()
-                    ->required()
+                    ->required(fn (string $operation): bool => $operation == 'create')
                     ->maxLength(255),
                 Forms\Components\Select::make('user_type')
                     ->required()
--- resources/views/users/includes/common/profile-journalist.blade.php
+++ resources/views/users/includes/common/profile-journalist.blade.php
@@ -21,35 +21,35 @@
 			</div>
 			<div class="col-12">
 				@if($viewAs == 'architect')
-				<livewire:architects.pitch-stories.journalist-view :journalist="$journalist" />
+					<livewire:architects.pitch-stories.journalist-view :journalist="$journalist" />
 				@elseif ($viewAs == 'journalist' && $journalist->user_id === auth()->id())
-				<div class="d-grid">
-					<a href="{{ route('journalist.account.profile.setting.personal-info') }}" class="btn btn-primary fw-medium">Edit Profile</a>
-				</div>
+					<div class="d-grid">
+						<a href="{{ route('journalist.account.profile.setting.personal-info') }}" class="btn btn-primary fw-medium">Edit Profile</a>
+					</div>
 				@endif
 			</div>
 			@if ($journalist->location)
-			<div class="col-12">
-				<div class="row g-2">
-					<div class="col-auto">
-						<svg xmlns="http://www.w3.org/2000/svg" height="19" fill="currentColor" class="bi bi-geo-alt text-secondary" viewBox="0 0 16 16">
-							<path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
-							<path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-						</svg>
-					</div>
-					<div class="col">
-						@if($journalist->location->city() && $journalist->location->city()->first())
-							<span class="text-gray-700 bg-gray-200 badge rounded-pill text-capitalize">
-								{{ $journalist->location->city()->first()->state->country->name }}
-							</span>
-							<span class="text-gray-700 bg-gray-200 badge rounded-pill text-capitalize">
-								{{ $journalist->location->city()->first()->state->name }}
-							</span>
-						@endif
-						<span class="text-gray-700 bg-gray-200 badge rounded-pill">{{ $journalist->location->name }}</span>
+				<div class="col-12">
+					<div class="row g-2">
+						<div class="col-auto">
+							<svg xmlns="http://www.w3.org/2000/svg" height="19" fill="currentColor" class="bi bi-geo-alt text-secondary" viewBox="0 0 16 16">
+								<path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+								<path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+							</svg>
+						</div>
+						<div class="col">
+							@if($journalist->location->city() && $journalist->location->city()->first())
+								<span class="text-gray-700 bg-gray-200 badge rounded-pill text-capitalize">
+									{{ $journalist->location->city()->first()->state->country->name }}
+								</span>
+								<span class="text-gray-700 bg-gray-200 badge rounded-pill text-capitalize">
+									{{ $journalist->location->city()->first()->state->name }}
+								</span>
+							@endif
+							<span class="text-gray-700 bg-gray-200 badge rounded-pill">{{ $journalist->location->name }}</span>
+						</div>
 					</div>
 				</div>
-			</div>
 			@endif
 			<div class="col-12">
 				<div class="row g-2">
