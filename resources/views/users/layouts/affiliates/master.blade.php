b91aae66c6e6dd7d876aea3fac0dda3a7c80a49b
Aman Saini <amansaini87@rediffmail.com>
2024-06-14 00:43:21 +0530

    changed favicon

--- resources/views/users/layouts/affiliates/master.blade.php
+++ resources/views/users/layouts/affiliates/master.blade.php
@@ -5,6 +5,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		{!! seo($SEOData ?? null) !!}
 		@yield('head')
 		@stack('meta')
 		<link rel="icon" type="image/png" href="{{ asset(env('COMPANY_ICON')) }}">
