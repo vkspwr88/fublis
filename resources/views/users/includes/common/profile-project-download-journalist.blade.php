02d5d9ffa54251e36d609c9e27775bf11d0b0647
Aman Saini <amansaini87@rediffmail.com>
2024-07-05 20:07:45 +0530

    fix photograph zip files for project

--- resources/views/users/includes/common/profile-project-download-journalist.blade.php
+++ resources/views/users/includes/common/profile-project-download-journalist.blade.php
@@ -23,12 +23,16 @@
 				<p class="m-0 text-dark fs-6">Photographs</p>
 			</div>
 			<div class="col text-end">
-				<form class="p-0 m-0" action="{{ route('journalist.download.bulk', ['mediaKit' => $mediaKit->slug]) }}" method="post">
-					@csrf
-					<input type="hidden" value="photographs" name="file">
-					<input type="hidden" name="type" value="Photographs">
-					<button type="submit" class="btn btn-primary fs-6 fw-medium">Download</button>
-				</form>
+				@if($mediaKit->story->photographs_link)
+					<a class="btn btn-primary fs-6 fw-medium" href="{{ $mediaKit->story->photographs_link }}" target="_blank">Download</a>
+				@else
+					<form class="p-0 m-0" action="{{ route('journalist.download.bulk', ['mediaKit' => $mediaKit->slug]) }}" method="post">
+						@csrf
+						<input type="hidden" value="photographs" name="file">
+						<input type="hidden" name="type" value="Photographs">
+						<button type="submit" class="btn btn-primary fs-6 fw-medium">Download</button>
+					</form>
+				@endif
 			</div>
 		</div>
 	@endif
