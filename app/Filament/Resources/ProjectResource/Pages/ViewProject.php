e1e3cf7964032f1389fd87123deaf764db622269
Aman Saini <amansaini87@rediffmail.com>
2024-10-25 20:15:00 +0530

    add download media kit in view project

--- app/Filament/Resources/ProjectResource/Pages/ViewProject.php
+++ app/Filament/Resources/ProjectResource/Pages/ViewProject.php
@@ -4,8 +4,11 @@ namespace App\Filament\Resources\ProjectResource\Pages;
 
 use App\Filament\Resources\ProjectResource;
 use App\Http\Controllers\Admin\ProjectController;
+use App\Models\Project;
+use App\Services\DownloadService;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class ViewProject extends ViewRecord
 {
@@ -14,7 +17,45 @@ class ViewProject extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\EditAction::make(),
+            Actions\ActionGroup::make([
+				Actions\Action::make('download1')
+					// ->icon('heroicon-m-arrow-down-tray')
+					->label('Download Description')
+					->hidden(fn(Project $project) => !$project->project_doc_path)
+					->action(
+						function (Project $project) {
+							$mediaKit = $project->mediaKit[0];
+							// dd($mediaKit, $project);
+							$downloadService = new DownloadService;
+							return $downloadService->singleFileDownload($mediaKit->slug, $project->project_doc_path, 'Description');
+							// downloadService->singleFileDownload($mediaKit->slug, $request->file, $request->type);
+							// downloadService->zipFilesDownload($mediaKit, $request->file, $request->type);
+						}
+					),
+				Actions\Action::make('download2')
+					// ->icon('heroicon-m-arrow-down-tray')
+					->label('Download Photographs')
+					->hidden(fn(Project $project) => !($project->photographs && $project->photographs->where('image_type', 'photographs')->count() > 0))
+					->action(
+						function (Project $project) {
+							$mediaKit = $project->mediaKit[0];
+							$downloadService = new DownloadService;
+							return $downloadService->zipFilesDownload($mediaKit, 'photographs', 'Photographs');
+						}
+					),
+				Actions\Action::make('download3')
+					// ->icon('heroicon-m-arrow-down-tray')
+					->label('Download Diagrams')
+					->hidden(fn(Project $project) => !($project->photographs && $project->photographs->where('image_type', 'drawings')->count() > 0))
+					->action(
+						function (Project $project) {
+							$mediaKit = $project->mediaKit[0];
+							$downloadService = new DownloadService;
+							return $downloadService->zipFilesDownload($mediaKit, 'drawings', 'Drawings');
+						}
+					),
+			]),
+			Actions\EditAction::make(),
         ];
     }
 
