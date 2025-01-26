af079ae15da8808600013121d15b197875405277
Aman Saini <amans@ephpsolutions.com>
2025-01-26 20:34:15 +0530

    fixed [2025-01-21 17:20:46] Attempt to read property state on null

--- resources/views/users/includes/common/profile-architect.blade.php
+++ resources/views/users/includes/common/profile-architect.blade.php
@@ -28,30 +28,42 @@
 			</div>
 			@endif
 			@if ($architect->location)
-			<div class="col-12">
-				<div class="row g-2">
-					<div class="col-auto">
-						<svg xmlns="http://www.w3.org/2000/svg" height="19" fill="currentColor" class="bi bi-geo-alt text-secondary" viewBox="0 0 16 16">
-							<path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
-							<path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-						</svg>
-					</div>
-					<div class="col">
-						@php
-							$city = $architect->location->city()->first();
-						@endphp
-						<span class="text-gray-700 bg-gray-200 badge rounded-pill text-capitalize">
-							{{ $city->state->country->name }}
-						</span>
-						<span class="text-gray-700 bg-gray-200 badge rounded-pill text-capitalize">
-							{{ $city->state->name }}
-						</span>
-						{{-- <span class="text-gray-700 bg-gray-200 badge rounded-pill">{{ $architect->location->state->country->name }}</span>
-						<span class="text-gray-700 bg-gray-200 badge rounded-pill">{{ $architect->location->state->name }}</span> --}}
-						<span class="text-gray-700 bg-gray-200 badge rounded-pill">{{ $architect->location->name }}</span>
+				@php
+					$city = $architect->location->city()->first();
+					$stateName = null;
+					$countryName = null;
+					if($city && isset($city->state)){
+						$stateName = $city->state->name;
+						if(isset($city->state->country)){
+							$countryName = $city->state->country->name;
+						}
+					}
+				@endphp
+				<div class="col-12">
+					<div class="row g-2">
+						<div class="col-auto">
+							<svg xmlns="http://www.w3.org/2000/svg" height="19" fill="currentColor" class="bi bi-geo-alt text-secondary" viewBox="0 0 16 16">
+								<path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+								<path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+							</svg>
+						</div>
+						<div class="col">
+							@if($countryName)
+							<span class="text-gray-700 bg-gray-200 badge rounded-pill text-capitalize">
+								{{ $city->state->country->name }}
+							</span>
+							@endif
+							@if ($stateName)
+							<span class="text-gray-700 bg-gray-200 badge rounded-pill text-capitalize">
+								{{ $city->state->name }}
+							</span>
+							@endif
+							{{-- <span class="text-gray-700 bg-gray-200 badge rounded-pill">{{ $architect->location->state->country->name }}</span>
+							<span class="text-gray-700 bg-gray-200 badge rounded-pill">{{ $architect->location->state->name }}</span> --}}
+							<span class="text-gray-700 bg-gray-200 badge rounded-pill">{{ $architect->location->name }}</span>
+						</div>
 					</div>
 				</div>
-			</div>
 			@endif
 			@if ($architect->company->category)
 			<div class="col-12">
