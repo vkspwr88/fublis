b538bf07ee2291f92ac09f11bfce21b103358b1c
Aman Saini <amans@ephpsolutions.com>
2025-02-02 18:44:19 +0530

    added export for architect, journalist and pitches

--- app/Filament/Resources/ArchitectResource/Pages/ListArchitects.php
+++ app/Filament/Resources/ArchitectResource/Pages/ListArchitects.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\ArchitectResource\Pages;
 
+use App\Filament\Exports\ArchitectExporter;
 use App\Filament\Resources\ArchitectResource;
+use App\Models\Architect;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ListArchitects extends ListRecords
 {
@@ -13,8 +18,36 @@ class ListArchitects extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+			ExportAction::make()
+				->exports([
+					ExcelExport::make()
+						->withColumns([
+							Column::make('slug'),
+							Column::make('user.name')
+								->heading('Name'),
+							Column::make('user.email')
+								->heading('Email'),
+							Column::make('company.name')
+								->heading('Studio'),
+							Column::make('user_role')
+								->formatStateUsing(function (Architect $record): string {
+									return $record->user_role->label();
+								}),
+							Column::make('position.name')
+								->heading('Position'),
+							Column::make('location.name'),
+							Column::make('about_me'),
+							Column::make('created_at')
+								->heading('Create Date'),
+							Column::make('updated_at')
+								->heading('Last Update Date'),
+						])
+						->askForWriterType()
+				]),
+			/* Actions\ExportAction::make()
+				->exporter(ArchitectExporter::class), */
             Actions\CreateAction::make()
-								->label('New Architect'),
+				->label('New Architect'),
         ];
     }
 }
