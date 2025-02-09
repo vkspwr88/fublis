2926dea5b16bc56f1dbcacdde60ffc9744ad7526
Aman Saini <amans@ephpsolutions.com>
2025-02-09 18:16:56 +0530

    implement should queue

--- app/Mail/User/Architect/SubscriptionMail.php
+++ app/Mail/User/Architect/SubscriptionMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SubscriptionMail extends Mailable
+class SubscriptionMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
