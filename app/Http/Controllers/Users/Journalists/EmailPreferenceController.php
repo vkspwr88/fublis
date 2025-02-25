3426f4e75f6ca6dab795723023a9c3581225dee4
root <root@vmi2279372.contaboserver.net>
2025-02-25 16:08:45 +0100

    created controller file

---
+++ app/Http/Controllers/Users/Journalists/EmailPreferenceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Users\Journalists;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EmailPreferenceController extends Controller
+{
+    //
+}
