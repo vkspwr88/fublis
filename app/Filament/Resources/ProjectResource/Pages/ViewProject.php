abf4918a8664497ef91bf6e9ec4eeab2c9a8ab51
Aman Saini <amans@ephpsolutions.com>
2025-04-20 22:57:00 +0530

    set limit to 5

--- app/Filament/Resources/ProjectResource/Pages/ViewProject.php
+++ app/Filament/Resources/ProjectResource/Pages/ViewProject.php
@@ -46,7 +46,7 @@ class ViewProject extends ViewRecord
 								->select('image_path')
 								->where('image_type', 'photographs')
 								->where('imaggable_id', $project->id)
-								->limit(1)
+								->limit(5)
 								->get();
 
 							$imagesPath = $images->pluck('image_path');
