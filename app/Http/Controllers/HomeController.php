374de7892754e48ea3f28ba6638c9c7b94e2b74c
Aman Saini <amans@ephpsolutions.com>
2025-04-20 20:56:23 +0530

    updated aman file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,13 +31,20 @@ class HomeController extends Controller
 
 	public function aman()
 	{
-		$images = Image::where('imaggable_id', '9e9126ce-7a36-4a5b-b5fd-a17454bf8d8e')->get();
+		$images = Image::query()
+			->where('image_type', 'photographs')
+			->where('imaggable_id', '9e9126ce-7a36-4a5b-b5fd-a17454bf8d8e')
+			->get();
+
+		$totalSize = 0;
 		foreach($images as $image) {
 			echo Storage::url($image->image_path);
-			echo '<br>';
-			echo Storage::size($image->image_path);
+			echo ' - ';
+			echo Storage::size($image->image_path) / (1024 * 1024) . ' MB';
 			echo '<hr>';
+			$totalSize += (int)Storage::size($image->image_path);
 		}
+		echo ($totalSize / (1024 * 1024)) . ' MB';
 		phpinfo();
 	}
 }
